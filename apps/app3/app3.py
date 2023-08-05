cbf94b6ac4d7ff2ac9b9569872bd391249b56824
HalefCaetano <halef_caetano@hotmail.com>
2023-08-04 20:16:03 -0700

    Day 24 and 25

---
+++ apps/app3/app3.py
@@ -0,0 +1,41 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    i = 31
+    while i <= 290:
+        pdf.line(10, i, 200, i)
+        i = i + 10
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        i = 21
+        while i <= 280:
+            pdf.line(10, i, 200, i)
+            i = i + 10
+
+
+pdf.output("output.pdf")
