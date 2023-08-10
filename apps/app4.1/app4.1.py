efa897186a93295e41c2985e4e989c2b7b9563e2
HalefCaetano <halef_caetano@hotmail.com>
2023-08-09 18:09:20 -0700

    Day 28

--- apps/app4.1/app4.1.py
+++ apps/app4.1/app4.1.py
@@ -11,4 +11,10 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=filename)
 
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 pdf.output(f"/Users/halefcaetano/Desktop/python/Learn-Python-in-60-days---Udemy/apps/app4.1/Files/output.pdf")
