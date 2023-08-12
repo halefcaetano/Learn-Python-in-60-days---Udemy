f164309d216fe9dcab892a7190182b9e6abbb518
HalefCaetano <halef_caetano@hotmail.com>
2023-08-11 17:48:00 -0700

    Day 27

--- apps/app5/main.py
+++ apps/app5/main.py
@@ -5,5 +5,8 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=2023-07-12&sortBy=publishe
       "apiKey=408a3d1dc3244c4785890da7bc8320d3"
 
 request = requests.get(url)
-content = request.text
-print(content)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
